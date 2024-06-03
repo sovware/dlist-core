650ee143ac60f6bc24974db0f38a5f3a0f063e24
nazmulhasan103 <nazmulhasan.wp@gmail.com>
2024-06-03 15:28:22 +0600

    Fixed single -  category, location, tag sidebar issue

--- elementor/widgets.php
+++ elementor/widgets.php
@@ -1313,7 +1313,7 @@ class dlist_videoPopup extends Widget_Base
             <div class="content_above">
                 <?php if ( $video_link['url'] ) { ?>
                     <a href="<?php echo esc_url($video_link['url']); ?>" class="video-iframe btn-play">
-                        <?php class_exists( 'Directorist_Base' ) ? directorist_icon( 'la la-youtube', true, 'btn-icon' ) : ''; ?>
+                        <?php directorist_icon( 'la la-youtube', true, 'btn-icon' ); ?>
                         <span><?php echo esc_attr($btn); ?></span>
                     </a>
                 <?php } ?>
@@ -3362,6 +3362,9 @@ class Dlist_SingleCat extends Widget_Base
                 'label'   => __('Show More Filter?', 'dlist-core'),
                 'type'    => Controls_Manager::SWITCHER,
                 'default' => 'no',
+                'condition' => [
+                    'sidebar!' => 'yes'
+                ]
             ]
         );
 
@@ -3558,29 +3561,33 @@ class Dlist_SingleCat extends Widget_Base
 
     protected function render()
     {
-        $settings        = $this->get_settings_for_display();
-        $default_types = $settings['default_types'];
-        $types = $settings['types'] ? implode( ',', $settings['types'] ) : '';
-        $header          = $settings['header'];
-        $filter          = 'yes' == $settings['filter'] ? $settings['filter'] : 'no';
-        $sidebar         = $settings['sidebar'];
-        $show_pagination = $settings['show_pagination'];
-        $title           = $settings['title'];
-        $layout          = $settings['layout'];
-        $number_cat      = $settings['number_cat'];
-        $row             = $settings['row'];
-        $cat             = $settings['cat'] ? implode(',', $settings['cat']) : '';
-        $location        = $settings['location'] ? implode(',', $settings['location']) : '';
-        $tag             = $settings['tag'] ? implode(',', $settings['tag']) : '';
-        $featured        = $settings['featured'];
-        $popular         = $settings['popular'];
-        $order_by        = $settings['order_by'];
-        $order_list      = $settings['order_list'];
-        $map_height      = $settings['map_height'];
-        $user            = $settings['user'];
-        $web             = 'yes' == $user ? $settings['link']['url'] : '';
+        $settings = $this->get_settings_for_display();
+        $user     = $settings['user'];
 
-        echo do_shortcode('[directorist_category view="' . esc_attr($layout) . '" orderby="' . esc_attr($order_by) . '" order="' . esc_attr($order_list) . '" listings_per_page="' . esc_attr($number_cat) . '" category="' . esc_attr($cat) . '" tag="' . esc_attr($tag) . '" location="' . esc_attr($location) . '" featured_only="' . esc_attr($featured) . '" popular_only="' . esc_attr($popular) . '" header="' . esc_attr($header) . '" header_title ="' . esc_attr($title) . '" columns="' . esc_attr($row) . '" action_before_after_loop="' . esc_attr($sidebar) . '" show_pagination="' . esc_attr($show_pagination) . '" advanced_filter="' . esc_attr($filter) . '" map_height="' . $map_height . '" display_preview_image="yes" logged_in_user_only="' . esc_attr($user) . '" redirect_page_url="' . esc_attr($web) . '" directory_type="' . $types . '" default_directory_type="' . $default_types . '"]');
+        $atts = [
+            'default_directory_type' => $settings['default_types'],
+            'directory_type'         => $settings['types'] ? implode( ',', $settings['types'] ) : '',
+            'header'                 => $settings['header'],
+            'advanced_filter'        => 'yes' === $settings['filter'] ? $settings['filter'] : 'no',
+            'sidebar'                => $settings['sidebar'],
+            'show_pagination'        => $settings['show_pagination'],
+            'header_title'           => $settings['title'],
+            'view'                   => $settings['layout'],
+            'listings_per_page'      => $settings['number_cat'],
+            'columns'                => $settings['row'],
+            'category'               => $settings['cat'] ? implode( ',', $settings['cat'] ) : '',
+            'location'               => $settings['location'] ? implode( ',', $settings['location'] ) : '',
+            'tag'                    => $settings['tag'] ? implode( ',', $settings['tag'] ) : '',
+            'featured_only'          => $settings['featured'],
+            'popular_only'           => $settings['popular'],
+            'orderby'                => $settings['order_by'],
+            'order'                  => $settings['order_list'],
+            'map_height'             => $settings['map_height'],
+            'logged_in_user_only'    => $user,
+            'redirect_page_url'      => 'yes' === $user ? $settings['link']['url'] : '',
+        ];
+
+        wpwax_run_shortcode( 'directorist_category', $atts );
     }
 }
 
@@ -3879,6 +3886,9 @@ class Dlist_SingleLoc extends Widget_Base
                 'label'   => __('Show More Filter?', 'dlist-core'),
                 'type'    => Controls_Manager::SWITCHER,
                 'default' => 'no',
+                'condition' => [
+                    'sidebar!' => 'yes'
+                ]
             ]
         );
 
@@ -4076,29 +4086,33 @@ class Dlist_SingleLoc extends Widget_Base
 
     protected function render()
     {
-        $settings        = $this->get_settings_for_display();
-        $header          = $settings['header'];
-        $default_types = $settings['default_types'];
-        $types = $settings['types'] ? implode( ',', $settings['types'] ) : '';
-        $filter          = 'yes' == $settings['filter'] ? $settings['filter'] : 'no';
-        $sidebar         = $settings['sidebar'];
-        $show_pagination = $settings['show_pagination'];
-        $title           = $settings['title'];
-        $layout          = $settings['layout'];
-        $number_cat      = $settings['number_cat'];
-        $row             = $settings['row'];
-        $cat             = $settings['cat'] ? implode(',', $settings['cat']) : '';
-        $location        = $settings['location'] ? implode(',', $settings['location']) : '';
-        $tag             = $settings['tag'] ? implode(',', $settings['tag']) : '';
-        $featured        = $settings['featured'];
-        $popular         = $settings['popular'];
-        $order_by        = $settings['order_by'];
-        $order_list      = $settings['order_list'];
-        $map_height      = $settings['map_height'];
-        $user            = $settings['user'];
-        $web             = 'yes' == $user ? $settings['link']['url'] : '';
+        $settings = $this->get_settings_for_display();
+        $user     = $settings['user'];
 
-        echo do_shortcode('[directorist_location view="' . esc_attr($layout) . '" orderby="' . esc_attr($order_by) . '" order="' . esc_attr($order_list) . '" listings_per_page="' . esc_attr($number_cat) . '" category="' . esc_attr($cat) . '" tag="' . esc_attr($tag) . '" location="' . esc_attr($location) . '" featured_only="' . esc_attr($featured) . '" popular_only="' . esc_attr($popular) . '" header="' . esc_attr($header) . '" header_title ="' . esc_attr($title) . '" columns="' . esc_attr($row) . '" action_before_after_loop="' . esc_attr($sidebar) . '" show_pagination="' . esc_attr($show_pagination) . '" advanced_filter="' . esc_attr($filter) . '" map_height="' . $map_height . '" display_preview_image="yes" logged_in_user_only="' . esc_attr($user) . '" redirect_page_url="' . esc_attr($web) . '" directory_type="' . $types . '" default_directory_type="' . $default_types . '"]');
+        $atts = [
+            'default_directory_type' => $settings['default_types'],
+            'directory_type'         => $settings['types'] ? implode( ',', $settings['types'] ) : '',
+            'header'                 => $settings['header'],
+            'advanced_filter'        => 'yes' === $settings['filter'] ? $settings['filter'] : 'no',
+            'sidebar'                => $settings['sidebar'],
+            'show_pagination'        => $settings['show_pagination'],
+            'header_title'           => $settings['title'],
+            'view'                   => $settings['layout'],
+            'listings_per_page'      => $settings['number_cat'],
+            'columns'                => $settings['row'],
+            'category'               => $settings['cat'] ? implode( ',', $settings['cat'] ) : '',
+            'location'               => $settings['location'] ? implode( ',', $settings['location'] ) : '',
+            'tag'                    => $settings['tag'] ? implode( ',', $settings['tag'] ) : '',
+            'featured_only'          => $settings['featured'],
+            'popular_only'           => $settings['popular'],
+            'orderby'                => $settings['order_by'],
+            'order'                  => $settings['order_list'],
+            'map_height'             => $settings['map_height'],
+            'logged_in_user_only'    => $user,
+            'redirect_page_url'      => 'yes' === $user ? $settings['link']['url'] : '',
+        ];
+
+        wpwax_run_shortcode( 'directorist_location', $atts );
     }
 }
 
@@ -4396,6 +4410,9 @@ class Dlist_SingleTag extends Widget_Base
                 'label'   => __('Show More Filter?', 'dlist-core'),
                 'type'    => Controls_Manager::SWITCHER,
                 'default' => 'no',
+                'condition' => [
+                    'sidebar!' => 'yes'
+                ]
             ]
         );
 
@@ -4593,29 +4610,33 @@ class Dlist_SingleTag extends Widget_Base
 
     protected function render()
     {
-        $settings        = $this->get_settings_for_display();
-        $header          = $settings['header'];
-        $default_types = $settings['default_types'];
-        $types = $settings['types'] ? implode( ',', $settings['types'] ) : '';
-        $filter          = 'yes' == $settings['filter'] ? $settings['filter'] : 'no';
-        $sidebar         = $settings['sidebar'];
-        $show_pagination = $settings['show_pagination'];
-        $title           = $settings['title'];
-        $layout          = $settings['layout'];
-        $number_cat      = $settings['number_cat'];
-        $row             = $settings['row'];
-        $cat             = $settings['cat'] ? implode(',', $settings['cat']) : '';
-        $location        = $settings['location'] ? implode(',', $settings['location']) : '';
-        $tag             = $settings['tag'] ? implode(',', $settings['tag']) : '';
-        $featured        = $settings['featured'];
-        $popular         = $settings['popular'];
-        $order_by        = $settings['order_by'];
-        $order_list      = $settings['order_list'];
-        $map_height      = $settings['map_height'];
-        $user            = $settings['user'];
-        $web             = 'yes' == $user ? $settings['link']['url'] : '';
+        $settings = $this->get_settings_for_display();
+        $user     = $settings['user'];
 
-        echo do_shortcode('[directorist_tag view="' . esc_attr($layout) . '" orderby="' . esc_attr($order_by) . '" order="' . esc_attr($order_list) . '" listings_per_page="' . esc_attr($number_cat) . '" category="' . esc_attr($cat) . '" tag="' . esc_attr($tag) . '" location="' . esc_attr($location) . '" featured_only="' . esc_attr($featured) . '" popular_only="' . esc_attr($popular) . '" header="' . esc_attr($header) . '" header_title ="' . esc_attr($title) . '" columns="' . esc_attr($row) . '" action_before_after_loop="' . esc_attr($sidebar) . '" show_pagination="' . esc_attr($show_pagination) . '" advanced_filter="' . esc_attr($filter) . '" map_height="' . $map_height . '" display_preview_image="yes" logged_in_user_only="' . esc_attr($user) . '" redirect_page_url="' . esc_attr($web) . '" directory_type="' . $types . '" default_directory_type="' . $default_types . '"]');
+        $atts = [
+            'default_directory_type' => $settings['default_types'],
+            'directory_type'         => $settings['types'] ? implode( ',', $settings['types'] ) : '',
+            'header'                 => $settings['header'],
+            'advanced_filter'        => 'yes' === $settings['filter'] ? $settings['filter'] : 'no',
+            'sidebar'                => $settings['sidebar'],
+            'show_pagination'        => $settings['show_pagination'],
+            'header_title'           => $settings['title'],
+            'view'                   => $settings['layout'],
+            'listings_per_page'      => $settings['number_cat'],
+            'columns'                => $settings['row'],
+            'category'               => $settings['cat'] ? implode( ',', $settings['cat'] ) : '',
+            'location'               => $settings['location'] ? implode( ',', $settings['location'] ) : '',
+            'tag'                    => $settings['tag'] ? implode( ',', $settings['tag'] ) : '',
+            'featured_only'          => $settings['featured'],
+            'popular_only'           => $settings['popular'],
+            'orderby'                => $settings['order_by'],
+            'order'                  => $settings['order_list'],
+            'map_height'             => $settings['map_height'],
+            'logged_in_user_only'    => $user,
+            'redirect_page_url'      => 'yes' === $user ? $settings['link']['url'] : '',
+        ];
+
+        wpwax_run_shortcode( 'directorist_tag', $atts );
     }
 }
 
@@ -5131,7 +5152,7 @@ class dlist_Subscribe extends Widget_Base
                             <div class="col-lg-8 offset-lg-2 col-md-10 offset-md-1 col-sm-8 offset-sm-2">
                                 <form action="<?php echo esc_url($action); ?>" method="get" class="subscribe-form m-top-40">
                                     <div class="form-group">
-                                        <?php class_exists( 'Directorist_Base' ) ? directorist_icon( 'las la-envelope-o' ) : ''; ?>
+                                        <?php directorist_icon( 'las la-envelope-o' ); ?>
                                         <input type="email" placeholder="<?php echo esc_attr_x('Enter your email', 'placeholder', 'dlist-core'); ?>" value="" name="EMAIL" class="required email" id="mce-EMAIL" required>
                                     </div>
                                     <input type="submit" value="<?php echo esc_attr($btn); ?>" class="btn btn-gradient btn-gradient-one">
